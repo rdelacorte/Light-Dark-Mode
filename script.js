cc3caaebb405de764c1170b7018b45572b9393b5
Rafael Dela Corte <101639966+rdelacorte@users.noreply.github.com>
2022-11-16 13:27:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function changeMode () {
+    changeClasses();
+    changeText();
+    
+    
+}
+
+
+function changeClasses() {
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+
+
+}
+
+
+function changeText() {
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+
+    if(body.classList.contains(darkModeClass)) {
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " ON";
+    return;
+    }
+
+    button.innerHTML = darkMode;
+    h1.innerHTML = lightMode + " ON";
+    
+    
+
+
+}
+
+
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('botao');
+const h1 = document.getElementById('text');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+
+console.log(body);
+console.log(button);
+console.log(footer);
+
+button.addEventListener('click', changeMode);
